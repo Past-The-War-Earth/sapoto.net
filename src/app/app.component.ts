483d79f4f362939222bab1dfa79c9c001362131b
russoturisto <russoturisto@gmail.com>
2022-01-29 09:14:38 -0700

    Added menu icons and made them transparent.  Changed menu items.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,86 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+  public metaCategories = [
+    {
+      categories: [
+        'Baby',
+        'Toddler',
+        'Young Childhood',
+        'Middle Childhood',
+        'Late Childhood',
+        'Teens',
+        'Adults'
+      ],
+      image: 'Children',
+      title: 'Children',
+      url: '/folder/Children',
+    },
+    {
+      categories: [
+        'Siblings',
+        'Cousins',
+        '2nd Cousins',
+        '3rd Cousins'
+      ],
+      image: 'Siblings_n_Kin',
+      title: 'Sibligs & Kin',
+      url: '/folder/Children',
+    },
+    {
+      categories: [
+        'Young',
+        'Working',
+        'Retired',
+        'Grandparents',
+        'Great Grandparents'
+      ],
+      image: 'Parents',
+      title: 'Parents',
+      url: '/folder/Parents',
+    },
+    {
+      categories: [
+        'Dating',
+        'Engaged',
+        'Married > 5 years',
+        'Married 5 to 10 years',
+        'Married 10+ years'
+      ],
+      image: 'Partners',
+      title: 'Partners',
+      url: '/folder/Partners',
+    },
+    { 
+      categories: [
+        'Nephews & Nieces',
+        'Aunts & Uncles',
+        'Grand Auns & Uncles',
+        'Grand nephews & nieces'   
+      ],
+      image: 'Extended',
+      title: 'Extended',
+      url: '/folder/Extended',
+    },
+    { 
+      categories: [
+        'Best friends',
+        'Close friends',
+        'Good friends',
+        'New friends',
+        'Acquaintances'
+      ],
+      image: 'Friends',
+      title: 'Friends',
+      url: '/folder/Friends',
+     },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  public labels = ['Bed Time', 'Homework', 'Chores', 'Cleaning', 'Hygene'];
+  constructor() { }
+
+  deSpace(
+    name: string
+  ) {
+    name.replace(' ', '_')
+  }
 }
