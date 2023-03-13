0f99939ef7d20f7d402442e850e4473cf908d45d
Artem Shamsutdinov <Artem.V.Shamsutdinov@dataindependence.net>
2023-03-13 16:50:57 -0600

    Removed generated artifacts

--- apps/core/src/generated/qInterfaces.ts
+++
@@ -1,5 +0,0 @@
-export * from './query/QSituation';
-export * from './query/QSituationRating';
-export * from './query/QTheme';
-export * from './query/QTopic';
-export * from './query/QUserTopicFavorite';
--- apps/core/src/generated/vInterfaces.ts
+++
@@ -1,5 +0,0 @@
-export * from './validation/VSituation';
-export * from './validation/VSituationRating';
-export * from './validation/VTheme';
-export * from './validation/VTopic';
-export * from './validation/VUserTopicFavorite';
